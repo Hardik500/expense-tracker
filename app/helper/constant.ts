a18e01ae6d31599e9feb930fe46c9424fc437a36
Hardik Khandelwal <hardikkhandelwal500@gmail.com>
2024-06-08 22:49:29 +0530

    feat: Add constant for fields list
    This commit adds a new constant called FIELDS_LIST in the helper/constant.ts file. The constant defines a list of fields with their labels, validity, and types. This constant will be used for data filtering and validation in the application.

---
+++ app/helper/constant.ts
@@ -0,0 +1,37 @@
+export const FIELDS_LIST = {
+    DATE: {
+        label: 'Date',
+        isValid: true,
+        type: 'date'
+    },
+    DESCRIPTION: {
+        label: 'Description',
+        isValid: true,
+        type: 'string'
+    },
+    UNKNOWN: {
+        label: 'Unknown',
+        isValid: false,
+        type: 'string'
+    },
+    CATEGORY: {
+        label: 'Category',
+        isValid: true,
+        type: 'string'
+    },
+    CREDIT: {
+        label: 'Credit',
+        isValid: true,
+        type: 'number'
+    },
+    DEBIT: {
+        label: 'Debit',
+        isValid: true,
+        type: 'number'
+    },
+    BALANCE: {
+        label: 'Balance',
+        isValid: true,
+        type: 'number'
+    }
+};
