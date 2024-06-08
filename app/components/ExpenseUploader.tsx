89af8900eaac72b84317db95fefabaf651100675
Hardik Khandelwal <hardikkhandelwal500@gmail.com>
2024-06-08 19:59:18 +0530

    chore: Improve ExpenseUploader UI and functionality

--- app/components/ExpenseUploader.tsx
+++ app/components/ExpenseUploader.tsx
@@ -10,6 +10,10 @@ interface ExpenseUploaderProps {
 }
 
 export default function ExpenseUploader({ setFiles }: ExpenseUploaderProps) {
+    const onInputClick = (e: { target: { value: string; }; }) => {
+        e.target.value = '';
+    }
+
     return (
         <div className="">
             <Form action="/expenses" method="post" encType="multipart/form-data">
@@ -20,7 +24,7 @@ export default function ExpenseUploader({ setFiles }: ExpenseUploaderProps) {
                     </button>
                 </div>
                 <div className="flex items-center justify-center w-full">
-                    <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
+                    <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                         <div className="flex flex-col items-center justify-center pt-5 pb-6">
                             <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
@@ -28,7 +32,7 @@ export default function ExpenseUploader({ setFiles }: ExpenseUploaderProps) {
                             <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
                             <p className="text-xs text-gray-500 dark:text-gray-400">XLS, CSV OR XLSX</p>
                         </div>
-                        <input id="dropzone-file" name="file" type="file" className="hidden" onChange={(event) => setFiles(event.target.files)} multiple/>
+                        <input id="dropzone-file" name="file" type="file" className="hidden" onChange={(event) => setFiles(event.target.files)} onClick={onInputClick} multiple />
                     </label>
                 </div>
             </Form>
