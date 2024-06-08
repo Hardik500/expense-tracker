497e14c0188798fbf3ac1f8a7b9e1fa1e020fa66
Hardik Khandelwal <hardikkhandelwal500@gmail.com>
2024-06-08 13:06:50 +0530

    chore: Add Tailwind CSS configuration and styles

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,18 @@
-import type { MetaFunction } from "@remix-run/node";
+import { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    {
+      property: "title",
+      content: "Expense Tracker",
+    },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="">
+      <p>Expense Tracker</p>
     </div>
   );
 }
